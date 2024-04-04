a146b986df0c40229de7866b708a59c28cf93bb2
Greg <gregoryminezzi@gmail.com>
2024-04-04 16:52:03 +0200

    implement scroll animations

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function createProjectsList(projectsList) {
     return ` 
     <div class="card">
       <div class="title-container">
-        <h3>${name}</h3>
+        <h3>${name}</h3> <a href="${live}" target="_blank"><i class="fa-solid fa-up-right-from-square"></i></a>
       </div>
       <div class="img-container">
-        <a href="${live}" target="_blank"><img src="${image}" alt="${name}" /></a>
+        <img src="${image}" alt="${name}" />
       </div>
       <div class="description">
         <div>${description}</div>
@@ -41,3 +41,43 @@ function createProjectsList(projectsList) {
   });
   projectsDiv.innerHTML = content.join("");
 }
+
+// --------------animations--------------
+
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const roles = document.getElementById("roles");
+const aboutDiv = document.getElementById("about");
+const about = document.getElementById("about-title");
+const projectsContainer = document.getElementById("projects");
+const projectsTitle = document.getElementById("projects-title");
+const contactDiv = document.getElementById("contact");
+const contact = document.getElementById("contact-title");
+
+const grow = (element, position) => {
+  element.style.fontSize = `${position + 100}px`;
+  element.style.opacity = `${1 - position / 500}`;
+};
+
+const scroll = (div, element, position, direction) => {
+  const divPosition = div.offsetTop;
+  const adjPosition = position + Math.floor(window.innerHeight * 0.75);
+  const speed = window.innerWidth * 0.0009;
+  console.log(speed)
+  if (adjPosition > divPosition) {
+    element.style.transform = `translateX(${
+      (adjPosition - divPosition) * speed * direction
+    }px)`;
+  }
+};
+
+document.addEventListener("scroll", function (e) {
+  position = window.scrollY;
+  window.requestAnimationFrame(function () {
+    grow(first, position);
+    grow(last, position);
+    scroll(aboutDiv, about, position, 1);
+    scroll(projectsContainer, projectsTitle, position, -1);
+    scroll(contactDiv, contact, position, 1);
+  });
+});
