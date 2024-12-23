afc0f2282497edd415a4d5522495a1bd844f22fa
Greg <gregoryminezzi@gmail.com>
2024-12-23 15:01:10 +0100

    edit animation with AOS instead

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createProjectsList(projectsList) {
     // });
 
     return ` 
-    <div class="card">
+    <div class="card"  data-aos="fade-up">
       <a href="${live}" target="_blank">
         <div class="title-container">
           <h3>${name}</h3><i class="fa-solid fa-up-right-from-square"></i>
@@ -46,44 +46,37 @@ function createProjectsList(projectsList) {
 
 // --------------animations--------------
 
-const first = document.getElementById("first");
-const last = document.getElementById("last");
-const roles = document.getElementById("roles");
-const aboutDiv = document.getElementById("about");
-const about = document.getElementById("about-title");
-const projectsContainer = document.getElementById("projects");
-const projectsTitle = document.getElementById("projects-title");
-const contactDiv = document.getElementById("contact");
-const contact = document.getElementById("contact-title");
+// const first = document.getElementById("first");
+// const last = document.getElementById("last");
 
-const grow = (element, position) => {
-  position += 80;
-  element.style.fontSize = `${position}px`;
-  element.style.opacity = `${1 - position / 500}`;
-};
+// const grow = (element, position) => {
+//   position += 80;
+//   element.style.fontSize = `${position}px`;
+//   element.style.opacity = `${1 - position / 500}`;
+// };
 
-const scroll = (div, element, position, direction) => {
-  const divPosition = div.offsetTop;
-  const adjPosition = position + Math.floor(window.innerHeight * 0.75);
-  const speed = window.innerWidth * 0.0009;
-  console.log(speed);
-  if (adjPosition > divPosition) {
-    element.style.transform = `translateX(${
-      (adjPosition - divPosition) * speed * direction
-    }px)`;
-  }
-};
+// const scroll = (div, element, position, direction) => {
+//   const divPosition = div.offsetTop;
+//   const adjPosition = position + Math.floor(window.innerHeight * 0.75);
+//   const speed = window.innerWidth * 0.0009;
+//   console.log(speed);
+//   if (adjPosition > divPosition) {
+//     element.style.transform = `translateX(${
+//       (adjPosition - divPosition) * speed * direction
+//     }px)`;
+//   }
+// };
 
-document.addEventListener("scroll", function (e) {
-  position = window.scrollY;
-  window.requestAnimationFrame(function () {
-    grow(first, position);
-    grow(last, position);
-    scroll(aboutDiv, about, position, 1);
-    scroll(projectsContainer, projectsTitle, position, -1);
-    scroll(contactDiv, contact, position, 1);
-  });
-});
+// document.addEventListener("scroll", function (e) {
+//   position = window.scrollY;
+//   window.requestAnimationFrame(function () {
+//     grow(first, position);
+//     grow(last, position);
+//     scroll(aboutDiv, about, position, 1);
+//     scroll(projectsContainer, projectsTitle, position, -1);
+//     scroll(contactDiv, contact, position, 1);
+//   });
+// });
 
 
 // Contact form submission
